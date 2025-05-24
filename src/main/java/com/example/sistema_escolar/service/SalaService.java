832e2bd3d96434a10c38415d88b45c7aa049773a
Jardel Santos <jardel.santos.23@outlook.com>
2025-05-24 11:09:08 -0300

    Atualização do métodos de criar aluno, criar professor, vincular aluno ao professor e desevinular aluno

--- src/main/java/com/example/sistema_escolar/service/SalaService.java
+++ src/main/java/com/example/sistema_escolar/service/SalaService.java
@@ -20,47 +20,72 @@ public class SalaService {
         this.professorRepository = professorRepository;
     }
 
+    //Criar aluno
+    public AlunoModel criarAluno(AlunoModel aluno){
+        return alunoRepository.save(aluno);
+    }
+
+    //Criar Professore
+    public ProfessorModel criarProfessor(ProfessorModel professor){
+        return professorRepository.save(professor);
+    }
+
     //Listar todos os alunos
     public List<AlunoModel> listarAlunos(){
         return alunoRepository.findAll();
     }
 
     //Listar todos os professores
-    public List<ProfessorModel> listarProf(){
+    public List<ProfessorModel> listarProfesores(){
         return professorRepository.findAll();
     }
 
     //Buscar alunos por id
-    public Optional<AlunoModel> encontrarAlunoId(Long id){
-        return alunoRepository.findById(id);
+    public Optional<AlunoModel> buscarAluno(Long alunoId){
+        return alunoRepository.findById(alunoId);
     }
 
     //Buscar professor por id
-    public Optional<ProfessorModel> encontrarProfId(Long id){
-        return professorRepository.findById(id);
+    public Optional<ProfessorModel> buscarProfessor(Long professorId){
+        return professorRepository.findById(professorId);
     }
 
-    //Deletar professor por id
-    public void deletarProf(Long id){
-        professorRepository.deleteById(id);
+    //Vincular aluno a professor
+    public void vincularAlunoAProfessor(Long professorId, Long alunoId){
+        ProfessorModel professor = professorRepository.findById(professorId)
+                .orElseThrow(()-> new RuntimeException("Professor não localizado"));
+
+        AlunoModel aluno = alunoRepository.findById(alunoId)
+                .orElseThrow(() -> new RuntimeException("Aluno não localizado"));
+
+        aluno.setProfessor(professor);
+        alunoRepository.save(aluno);
+    }
+
+    //Desvincular aluno de porfessor
+
+    public void desvincularAlunoDeProfessor(Long professorId, Long alunoId){
+        ProfessorModel professor = professorRepository.findById(professorId)
+            .orElseThrow(()-> new RuntimeException("Professor não localizado"));
+
+        AlunoModel aluno = alunoRepository.findById(alunoId)
+            .orElseThrow(()-> new RuntimeException("Aluno não localizado"));
+
+        if(!professor.getAlunos().contains(aluno)){
+            throw new RuntimeException("Aluno não esta matriculado ao professor");
+        }
+
+        //Desvincula aluno de um professor
+        professor.getAlunos().remove(aluno);
+
+        //Atualiza o objeto professor do banco de dados
+        professorRepository.save(professor);
+
+
     }
 
     //Deletar aluno por id
-    public void deletarAluno(Long id){
-        alunoRepository.deleteById(id);
-    }
 
-    // Metodo para adicionar alunos a professores
-    public void adicionarAlunoAProfessor(Long profId, Long alunoId){
-        ProfessorModel professor = professorRepository.findById(profId)
-                .orElseThrow(() -> new RuntimeException("Professor não encontrado"));
+    //Deletar professor por id
 
-        AlunoModel aluno = alunoRepository.findById(alunoId)
-                .orElseThrow(() -> new RuntimeException("Aluno não encontrado"));
-
-        //Atribui o professor ao aluno
-        aluno.setProfessor(professor);
-
-        alunoRepository.save(aluno);
-    }
 }
